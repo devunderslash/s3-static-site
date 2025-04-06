b0bf62b9827b56796f44d891d3ab6341e9737093
devunderslash <devunderslash@hotmail.com>
2025-04-06 22:20:19 +0100

    tidying terraform

--- terraform/deployments/us-east-1/prod/main.tf
+++ terraform/deployments/us-east-1/prod/main.tf
@@ -10,7 +10,7 @@ module "static_website_prod" {
 
 provider "aws" {
   region     = local.region
-  profile    = "Dev"
+  profile    = "prod"
   # access_key = var.aws_access_key
   # secret_key = var.aws_secret_key
 }
